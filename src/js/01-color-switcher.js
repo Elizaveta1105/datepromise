83557e2448b1758db212d6ff79e7457b009e9da6
Elizaveta <elizaveta@short.io>
2022-09-19 13:28:14 +0300

    added color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    body: document.querySelector("body"),
+    onStartBtn: document.querySelector("button[data-start]"),
+    onEndBtn: document.querySelector("button[data-stop]")
+}
 
+refs.onStartBtn.addEventListener("click", onChangeBackground)
+refs.onEndBtn.addEventListener("click", onStopBackground)
+
+let intervalID = 0;
+
+function onChangeBackground() {
+    intervalID = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+
+    refs.onStartBtn.disabled = true;
+}
+
+function onStopBackground() {
+    clearInterval(intervalID);
+
+    refs.onStartBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
